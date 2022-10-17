c594bfd19a6078e6ea6ea30c58c35245325e4ccd
D1g1talEntr0py <jason.dimeo@gmail.com>
2022-10-17 15:33:40 -0400

    Updates for importing modules -Renamed re-exports files to index.js and index.d.ts. -Removed references to MultiMap from SetMultiMap and WeakSetMultiMap. -Added "exports" field in package.json to be able to import individual modules to allow tree-shaking in esbuild.

--- src/set-multi-map.js
+++ src/set-multi-map.js
@@ -1,15 +1,15 @@
 /**
  *
- * @typedef {Map<*, *>} MultiMap
+ * @typedef {Map<*, *>} SetMultiMap
  * @extends Map
  */
 export default class SetMultiMap extends Map {
 	/**
-	 * Adds a new element with a specified key and value to the MultiMap. If an element with the same key already exists, the value will be added to the underlying {@link Set}.
+	 * Adds a new element with a specified key and value to the SetMultiMap. If an element with the same key already exists, the value will be added to the underlying {@link Set}.
 	 *
 	 * @param {*} key The key to set.
-	 * @param {*} value The value to add to the MultiMap
-	 * @returns {MultiMap} The MultiMap with the updated key and value.
+	 * @param {*} value The value to add to the SetMultiMap
+	 * @returns {SetMultiMap} The SetMultiMap with the updated key and value.
 	 */
 	set(key, value) {
 		super.set(key, (super.get(key) ?? new Set()).add(value));
--- src/weak-set-multi-map.js
+++ src/weak-set-multi-map.js
@@ -1,15 +1,15 @@
 /**
  *
- * @typedef {Map<*, *>} MultiMap
+ * @typedef {Map<*, *>} WeakSetMultiMap
  * @extends Map
  */
 export default class WeakSetMultiMap extends Map {
 	/**
-	 * Adds a new element with a specified key and value to the MultiMap. If an element with the same key already exists, the value will be added to the underlying {@link Set}.
+	 * Adds a new element with a specified key and value to the WeakSetMultiMap. If an element with the same key already exists, the value will be added to the underlying {@link Set}.
 	 *
 	 * @param {*} key The key to set.
-	 * @param {*} value The value to add to the MultiMap
-	 * @returns {MultiMap} The MultiMap with the updated key and value.
+	 * @param {*} value The value to add to the WeakSetMultiMap
+	 * @returns {WeakSetMultiMap} The WeakSetMultiMap with the updated key and value.
 	 */
 	set(key, value) {
 		super.set(key, (super.get(key) ?? new WeakSet()).add(value));
